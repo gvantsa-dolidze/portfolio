b5c7d8e82ce06760a1d503a8df5918ce882e6651
Gvantsa <gvantsa.dolidze@gmail.com>
2025-10-21 03:45:14 +0400

    changes

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -5,8 +5,8 @@ import {
   MapPin,
   Phone,
   Send,
-  Twitch,
   Twitter,
+  Facebook,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
@@ -56,10 +56,10 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Email</h4>
                   <a
-                    href="mailto:hello@gmail.com"
+                    href="mailto:gvantsa.dolidze@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    hello@gmail.com
+                    gvantsa.dolidze@gmail.com
                   </a>
                 </div>
               </div>
@@ -70,10 +70,10 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Phone</h4>
                   <a
-                    href="tel:+11234567890"
+                    href="tel:+995574483003"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    +1 (123) 456-7890
+                    +995 (574) 48-30-03
                   </a>
                 </div>
               </div>
@@ -84,7 +84,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Location</h4>
                   <a className="text-muted-foreground hover:text-primary transition-colors">
-                    Vancouver, BC, Canada
+                    Georgia, Tbilisi
                   </a>
                 </div>
               </div>
@@ -93,17 +93,23 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/gvantsadolidze"
+                  target="_blank"
+                >
                   <Linkedin />
                 </a>
-                <a href="#" target="_blank">
+                <a href="https://www.facebook.com/Gvantsa.93/" target="_blank">
+                  <Facebook />
+                </a>
+                <a href="https://x.com/Gvantsa_Dolidze" target="_blank">
                   <Twitter />
                 </a>
-                <a href="#" target="_blank">
+                <a
+                  href="https://www.instagram.com/gvantsadolidze/"
+                  target="_blank"
+                >
                   <Instagram />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitch />
                 </a>
               </div>
             </div>
@@ -130,7 +136,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Pedro Machado..."
+                  placeholder="Katherine..."
                 />
               </div>
 
@@ -148,7 +154,7 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com"
+                  placeholder="Katherine@gmail.com"
                 />
               </div>
 
